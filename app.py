d377dfa16fe926edaa418974e66dda909a30405b
prestyprajna <prestyprajna@gmail.com>
2021-03-21 15:44:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]  
+    # int_features = request.form.values()   
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    #output = round(prediction[0], 2)
+    
+    if (prediction == 1):
+        output = "YES"
+    else:
+        output = "NO"
+
+    return render_template('index.html', prediction_text="Home Loan Approved Status : " + output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
